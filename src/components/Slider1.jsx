af75a5e7090c0ddf68dcc3a67370410fbda85e9d
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2022-01-21 19:14:11 +0530

    slider update

--- src/components/Slider1.jsx
+++ src/components/Slider1.jsx
@@ -9,9 +9,9 @@ const Container=styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-   
+    
     position: relative;
-   
+   border: 3px solid blue;
 
 `
 const Arrow=styled.div`
@@ -20,37 +20,50 @@ const Arrow=styled.div`
     background-color: white;
     border-radius:50%;
     display: flex;
-    align-items: center;
+    align-items: center;  
     justify-content: center;
     position: absolute;
     top: 0;
     bottom:0;
-    margin: auto;
     left: ${(props)=>props.direction==="left" && "10px"};
     right: ${(props)=>props.direction==="right" && "10px"};
+    margin: auto;
     cursor: pointer ;
  
 `
 const Wrapper=styled.div`
     height: 100%;
+    border: 2px solid red;
 `
 const Slide=styled.div`
-    display: flex;
+    width: 100vw;
     height: 100vh;
-    width: 100vh;
+    display: flex;
     align-items: center;
+    border: 2px solid blue;
 
 `
 const ImgContainer=styled.div`
+    border: 2px solid green;
+    height:100%;
     flex:1;
-    /* height:100% */
+    
 
 
 `
 const InfoContainer=styled.div`
      flex:1;
+     padding:50px;
+    border: 2px solid violet;
+
 `
-const Image=styled.img``
+const Title=styled.h1``
+const Desc=styled.p``
+const Button=styled.button``
+const Image=styled.img`
+    height:80%;
+`
+
 
 const Slider1 = () => {
   return <Container>
@@ -59,16 +72,18 @@ const Slider1 = () => {
             </Arrow>
 
             <Wrapper>
+
            <Slide>
            <ImgContainer>
-                <Image src=""/>
+                <Image src="./images/img2.jpg"/>
             </ImgContainer>
-            
-            
-            <InfoContainer></InfoContainer>
+            <InfoContainer>
+                <Title>Supreme Sale</Title>
+                <Desc> get Flat 30% off !! Hurry Up</Desc>
+                <Button> SHOP NOW</Button>
+            </InfoContainer>
            </Slide>
             
-
             </Wrapper>
 
             <Arrow direction='right'>
